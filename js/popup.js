06a8761d322cf8796eef8cd6f992c1f67f194b8c
rakshith111 <rakshithbabu111@gmail.com>
2023-03-31 19:49:24 +0530

    Add Toggle button to switch between the two modes.

--- js/popup.js
+++ js/popup.js
@@ -9,16 +9,14 @@ const getMethods = (obj) => {
   );
 };
 
-
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const timezoneSelect = document.getElementById("timezoneSelect");
   const applyButton = document.getElementById("applyButton");
+  const toggleButton = document.getElementById("toggleButton");
 
   dayjs.extend(window.dayjs_plugin_timezone);
   dayjs.extend(window.dayjs_plugin_utc);
   dayjs.extend(window.dayjs_plugin_localizedFormat);
-
-
 
   // Populate the timezone select element
   const timezones = moment.tz.names();
@@ -29,16 +27,41 @@ document.addEventListener("DOMContentLoaded", () => {
     timezoneSelect.add(option);
   });
 
+  // Load the saved selected timezone and toggle state
+  const savedSettings = await browser.storage.local.get([
+    "selectedTimezone",
+    "toggleState",
+  ]);
+  if (savedSettings.selectedTimezone) {
+    timezoneSelect.value = savedSettings.selectedTimezone;
+  }
+  if (savedSettings.toggleState !== undefined) {
+    toggleButton.checked = savedSettings.toggleState;
+  }
+
   // Apply selected timezone and log the current time
-  applyButton.addEventListener("click", () => {
+  applyButton.addEventListener("click", async () => {
     const selectedTimezone = timezoneSelect.value;
     const currentTime = dayjs().tz(selectedTimezone).format("L LT");
     console.log("original time", dayjs().format("L LT"));
     console.log(`Current time in ${selectedTimezone}: ${currentTime}`);
+
+    // Save the selected timezone
+    await browser.storage.local.set({ selectedTimezone });
+  });
+
+  // Handle the toggle switch
+  toggleButton.addEventListener("change", async (event) => {
+    const toggleState = event.target.checked;
+    await browser.storage.local.set({ toggleState });
+
+    if (toggleState) {
+      console.log("Toggled to User Time Zone:", moment.tz.guess());
+    } else {
+      const selectedTimezone = timezoneSelect.value;
+      console.log(
+        `Toggled to switch to Selected Time Zone: ${selectedTimezone}`
+      );
+    }
   });
 });
-
-
-
-
-console.log("User Time Zone:",  moment.tz.guess());
