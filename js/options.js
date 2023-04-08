754497fbf68249e10626e162567957a161e2c2b7
MAX-dev2020 <60339025+MAX-dev2020@users.noreply.github.com>
2023-04-09 01:12:15 +0530

    added domains list

--- js/options.js
+++ js/options.js
@@ -2,51 +2,53 @@ document.getElementById('addDomain').addEventListener('click', addDomain);
 document.getElementById('removeSelected').addEventListener('click', removeSelected);
 
 function addDomain() {
-  const domain = prompt('Enter a domain URL to add:');
+  const domain = document.getElementById('disabledDomains').value;
   if (domain) {
-    const disabledDomains = document.getElementById('disabledDomains');
-    disabledDomains.value += '\n' + domain.trim();
+    const domainsList = document.getElementById('domainsList');
+    const listItem = document.createElement('li');
+    listItem.textContent = domain.trim();
+    listItem.addEventListener('click', () => {
+      listItem.classList.toggle('selected');
+    });
+    domainsList.appendChild(listItem);
+    saveOptions();
   }
 }
 
 function removeSelected() {
-  const whitelist = document.getElementById('whitelistedWebsites');
-  const selectedItems = whitelist.querySelectorAll('li.selected');
+  const domainsList = document.getElementById('domainsList');
+  const selectedItems = domainsList.querySelectorAll('li.selected');
   selectedItems.forEach(item => item.remove());
+  saveOptions();
 }
 
 function saveOptions() {
-  const disabledDomains = document.getElementById('disabledDomains').value;
-  const domainList = disabledDomains.split('\n').map(domain => domain.trim());
-  browser.storage.local.set({ disabledDomains: domainList }).then(refreshWhitelistedWebsites);
+  const domainsList = document.getElementById('domainsList');
+  const domainItems = domainsList.getElementsByTagName('li');
+  const domains = [];
+
+  for (let i = 0; i < domainItems.length; i++) {
+    domains.push(domainItems[i].textContent);
+  }
+
+  browser.storage.local.set({ disabledDomains: domains });
 }
 
 function restoreOptions() {
   browser.storage.local.get('disabledDomains')
     .then(result => {
       if (result.disabledDomains) {
-        document.getElementById('disabledDomains').value = result.disabledDomains.join('\n');
-      }
-      refreshWhitelistedWebsites();
-    });
-}
-
-function refreshWhitelistedWebsites() {
-  browser.storage.local.get('disabledDomains')
-    .then(result => {
-      const whitelist = document.getElementById('whitelistedWebsites');
-      whitelist.innerHTML = '';
-
-      if (result.disabledDomains) {
+        const domainsList = document.getElementById('domainsList');
         result.disabledDomains.forEach(domain => {
           const listItem = document.createElement('li');
           listItem.textContent = domain;
-          listItem.addEventListener('click', () => listItem.classList.toggle('selected'));
-          whitelist.appendChild(listItem);
+          listItem.addEventListener('click', () => {
+            listItem.classList.toggle('selected');
+          });
+          domainsList.appendChild(listItem);
         });
       }
     });
 }
 
 document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('saveOptions').addEventListener('click', saveOptions);
