7a39431481e3d36e2b4c4612055a49189002882a
rakshith111 <rakshithbabu111@gmail.com>
2023-04-12 12:12:46 +0530

    Make bg.js a  IIFE so its forced to load up

--- js/background.js
+++ js/background.js
@@ -1,30 +1,51 @@
-browser.runtime.onInstalled.addListener(() => {
-  browser.contextMenus.create({
-    id: "convert-date-time",
-    title: "Convert date and time",
-    contexts: ["selection"],
-  });
-});
+(function () {
+  function initializeBackgroundScript() {
+    // Create the context menu
+    browser.contextMenus.create({
+      id: "convert-date-time",
+      title: "Convert date and time",
+      contexts: ["selection"],
+    });
+    console.log("[b] Context menu created"); // Added log statement
 
-browser.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId === "convert-date-time") {
-    const selectedText = info.selectionText;
+    // Set up the event listener for context menu clicks
+    browser.contextMenus.onClicked.addListener(async (info, tab) => {
+      if (info.menuItemId === "convert-date-time") {
+        const selectedText = info.selectionText;
 
-    const settings = await browser.storage.local.get([
-      "selectedTimezone",
-      "toggleState",
-    ]);
+        const settings = await browser.storage.local.get([
+          "selectedTimezone",
+          "toggleState",
+        ]);
 
-    // Only use the saved timezone if the toggleState is not false
-    const currentSelectedTimezone =
-      settings.toggleState === false || settings.toggleState === undefined
-        ? settings.selectedTimezone || luxon.DateTime.local().zoneName
-        : luxon.DateTime.local().zoneName;
-
-    browser.tabs.sendMessage(tab.id, {
-      command: "convertDateTime",
-      text: selectedText,
-      targetTimezone: currentSelectedTimezone,
+        // Only use the saved timezone if the toggleState is not false
+        const currentSelectedTimezone =
+          settings.toggleState === false || settings.toggleState === undefined
+            ? settings.selectedTimezone || luxon.DateTime.local().zoneName
+            : luxon.DateTime.local().zoneName;
+        console.log("[b] Current selected timezone: ", currentSelectedTimezone);
+        browser.tabs
+          .sendMessage(tab.id, {
+            command: "manualConvertDateTime", // Change the command to 'manualConvertDateTime'
+            text: selectedText,
+            targetTimezone: currentSelectedTimezone,
+          })
+          .catch((error) => {
+            console.error("[p] Error sending message:", error);
+          });
+      }
     });
   }
-});
+
+  browser.runtime.onStartup.addListener(() => {
+    console.log("Background script started on browser startup");
+    initializeBackgroundScript();
+  });
+
+  browser.runtime.onInstalled.addListener((details) => {
+    console.log(
+      "Background script started on extension installation or update"
+    );
+    initializeBackgroundScript();
+  });
+})();
